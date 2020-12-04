126a3380f724399d17099f1c61c4b91d16313fdf
osymandius <bolistevens@gmail.com>
2020-12-04 11:47:29 +0000

    fit fixes

--- src/eth_fit/eth_fit.R
+++ src/eth_fit/eth_fit.R
@@ -7,51 +7,65 @@ asfr <- read.csv(paste0("depends/", tolower(iso3), "_dhs_asfr.csv"))
 # debugonce(make_model_frames)
 mf <- make_model_frames_dev(iso3, population, asfr,  areas, naomi_level =2, project=2020)
 
-TMB::compile("resources/tmb_nb_spike_eth_b.cpp")               # Compile the C++ file
-dyn.load(dynlib("resources/tmb_nb_spike_eth_b"))
+TMB::compile("resources/tmb_all_level_eth.cpp")               # Compile the C++ file
+dyn.load(dynlib("resources/tmb_all_level_eth"))
 
 tmb_int <- list()
 
-tmb_int$data <- list(M_obs = mf$district$M_obs,
-                     X_tips_dummy = mf$Z$X_tips_dummy,
-                     X_urban_dummy = mf$Z$X_urban_dummy,
-                     X_extract_dhs = mf$X_extract$X_extract_dhs,
-                     X_extract_ais = mf$X_extract$X_extract_ais,
-                     Z_tips = mf$Z$Z_tips,
-                     Z_tips_dhs = mf$Z$Z_tips_dhs,
-                     Z_age = mf$Z$Z_age,
-                     Z_period = mf$Z$Z_period,
-                     Z_spatial = mf$Z$Z_spatial,
-                     Z_interaction1 = sparse.model.matrix(~0 + id.interaction1, mf$mf_model),
-                     Z_interaction2 = sparse.model.matrix(~0 + id.interaction2, mf$mf_model),
-                     Z_interaction3 = sparse.model.matrix(~0 + id.interaction3, mf$mf_model),
-                     Z_country = mf$Z$Z_country,
-                     Z_omega1 = sparse.model.matrix(~0 + id.omega1, mf$mf_model),
-                     Z_omega2 = sparse.model.matrix(~0 + id.omega2, mf$mf_model),
-                     R_tips = mf$R$R_tips,
-                     R_age = mf$R$R_age,
-                     R_period = mf$R$R_period,
-                     R_spatial = mf$R$R_spatial,
-                     R_country = mf$R$R_country,
-                     rankdef_R_spatial = 1,
-                     log_offset_dhs = log(filter(mf$district$obs, ais_dummy ==0)$pys),
-                     births_obs_dhs = filter(mf$district$obs, ais_dummy ==0)$births,
-                     log_offset_ais = log(filter(mf$district$obs, ais_dummy ==1)$pys),
-                     births_obs_ais = filter(mf$district$obs, ais_dummy ==1)$births,
-                     pop = mf$mf_model$population,
-                     A_asfr_out = mf$out$A_asfr_out,
-                     A_tfr_out = mf$out$A_tfr_out,
-                     mics_toggle = mf$mics_toggle,
-                     out_toggle = mf$out_toggle,
-                     eth_toggle = 1,
-
-                     X_spike_2000_dhs = model.matrix(~0 + spike_2000, mf$district$obs %>% filter(ais_dummy==0)),
-                     X_spike_1999_dhs = model.matrix(~0 + spike_1999, mf$district$obs %>% filter(ais_dummy==0)),
-                     X_spike_2001_dhs = model.matrix(~0 + spike_2001, mf$district$obs %>% filter(ais_dummy==0)),
-                     
-                     X_spike_2000_ais = model.matrix(~0 + spike_2000, mf$district$obs %>% filter(ais_dummy == 1)),
-                     X_spike_1999_ais = model.matrix(~0 + spike_1999, mf$district$obs %>% filter(ais_dummy == 1)),
-                     X_spike_2001_ais = model.matrix(~0 + spike_2001, mf$district$obs %>% filter(ais_dummy == 1))
+tmb_int$data <- list(
+  M_naomi_obs = mf$M_naomi_obs,
+  M_full_obs = mf$M_full_obs,
+  X_tips_dummy = mf$Z$X_tips_dummy,
+  X_urban_dummy = mf$Z$X_urban_dummy,
+  X_extract_dhs = mf$X_extract$X_extract_dhs,
+  X_extract_ais = mf$X_extract$X_extract_ais,
+  X_extract_mics = mf$X_extract$X_extract_mics,
+  # Z_tips = mf$Z$Z_tips,
+  Z_tips_dhs = mf$Z$Z_tips_dhs,
+  Z_tips_ais = mf$Z$Z_tips_ais,
+  Z_age = mf$Z$Z_age,
+  Z_period = mf$Z$Z_period,
+  Z_spatial = mf$Z$Z_spatial,
+  Z_interaction1 = sparse.model.matrix(~0 + id.interaction1, mf$mf_model),
+  Z_interaction2 = sparse.model.matrix(~0 + id.interaction2, mf$mf_model),
+  Z_interaction3 = sparse.model.matrix(~0 + id.interaction3, mf$mf_model),
+  Z_country = mf$Z$Z_country,
+  Z_omega1 = sparse.model.matrix(~0 + id.omega1, mf$mf_model),
+  Z_omega2 = sparse.model.matrix(~0 + id.omega2, mf$mf_model),
+  R_tips = mf$R$R_tips,
+  R_age = mf$R$R_age,
+  R_period = mf$R$R_period,
+  R_spatial = mf$R$R_spatial,
+  R_country = mf$R$R_country,
+  rankdef_R_spatial = 1,
+  
+  log_offset_naomi = log(mf$observations$naomi_level_obs$pys),
+  births_obs_naomi = mf$observations$naomi_level_obs$births,
+  
+  log_offset_dhs = log(filter(mf$observations$full_obs, survtype == "DHS")$pys),
+  births_obs_dhs = filter(mf$observations$full_obs, survtype == "DHS")$births,
+  
+  log_offset_ais = log(filter(mf$observations$full_obs, survtype %in% c("AIS", "MIS"))$pys),
+  births_obs_ais = filter(mf$observations$full_obs, survtype %in% c("AIS", "MIS"))$births,
+  
+  pop = mf$mf_model$population,
+  # A_asfr_out = mf$out$A_asfr_out,
+  A_tfr_out = mf$out$A_tfr_out,
+  
+  A_full_obs = mf$observations$A_full_obs,
+  
+  mics_toggle = mf$mics_toggle,
+  
+  X_spike_2000_dhs = model.matrix(~0 + spike_2000, mf$observations$full_obs %>% filter(survtype == "DHS")),
+  X_spike_1999_dhs = model.matrix(~0 + spike_1999, mf$observations$full_obs %>% filter(survtype == "DHS")),
+  X_spike_2001_dhs = model.matrix(~0 + spike_2001, mf$observations$full_obs %>% filter(survtype == "DHS")),
+  
+  X_spike_2000_ais = model.matrix(~0 + spike_2000, mf$observations$full_obs %>% filter(survtype %in% c("AIS", "MIS"))),
+  X_spike_1999_ais = model.matrix(~0 + spike_1999, mf$observations$full_obs %>% filter(survtype %in% c("AIS", "MIS"))),
+  X_spike_2001_ais = model.matrix(~0 + spike_2001, mf$observations$full_obs %>% filter(survtype %in% c("AIS", "MIS")))
+  
+  # out_toggle = mf$out_toggle
+  # A_obs = mf$observations$A_obs,
 )
 
 tmb_int$par <- list(
@@ -59,8 +73,7 @@ tmb_int$par <- list(
   
   beta_tips_dummy = rep(0, ncol(mf$Z$X_tips_dummy)),
   beta_urban_dummy = rep(0, ncol(mf$Z$X_urban_dummy)),
-  
-  u_tips = rep(0, ncol(mf$Z$Z_tips)),
+  u_tips = rep(0, ncol(mf$Z$Z_tips_dhs)),
   log_prec_rw_tips = 0,
   
   u_age = rep(0, ncol(mf$Z$Z_age)),
@@ -72,22 +85,22 @@ tmb_int$par <- list(
   omega1 = array(0, c(ncol(mf$R$R_country), ncol(mf$Z$Z_age))),
   log_prec_omega1 = 0,
   lag_logit_omega1_phi_age = 0,
-
+  
   omega2 = array(0, c(ncol(mf$R$R_country), ncol(mf$Z$Z_period))),
   log_prec_omega2 = 0,
   lag_logit_omega2_phi_period = 0,
   
   u_period = rep(0, ncol(mf$Z$Z_period)),
   log_prec_rw_period = 0,
-lag_logit_phi_period = 0,
+  lag_logit_phi_period = 0,
   
   u_spatial_str = rep(0, ncol(mf$Z$Z_spatial)),
   log_prec_spatial = 0,
-  log_overdispersion = 0,
   
   beta_spike_2000 = 0,
   beta_spike_1999 = 0,
   beta_spike_2001 = 0,
+  log_overdispersion = 0,
   
   eta1 = array(0, c(ncol(mf$Z$Z_country), ncol(mf$Z$Z_period), ncol(mf$Z$Z_age))),
   log_prec_eta1 = 0,
@@ -113,26 +126,28 @@ tmb_int$random <- c("beta_0",
                     "beta_spike_2000",
                     "beta_spike_1999",
                     "beta_spike_2001",
-                    "omega1",
-                    "omega2",
                     "eta1",
                     "eta2",
-                    "eta3")
+                    "eta3",
+                    "omega1",
+                    "omega2"
+)
+
 if(mf$mics_toggle) {
-  tmb_int$data <- c(tmb_int$data, 
-                    "M_obs_mics" = mf$mics$M_obs_mics,
+  tmb_int$data <- c(tmb_int$data,
                     "Z_tips_mics" = mf$Z$Z_tips_mics,
                     "R_tips_mics" = mf$R$R_tips_mics,
-                    "births_obs_mics" = list(mf$mics$obs$births),
-                    "log_offset_mics" = list(log(mf$mics$obs$pys)),
-                    "A_mics" = mf$mics$A_mics,
-                    "X_spike_2000_mics" = list(model.matrix(~0 + spike_2000, mf$mics$obs)),
-                    "X_spike_1999_mics" = list(model.matrix(~0 + spike_1999, mf$mics$obs)),
-                    "X_spike_2001_mics" = list(model.matrix(~0 + spike_2001, mf$mics$obs))
-                    )
+                    "log_offset_mics" = list(log(filter(mf$observations$full_obs, survtype == "MICS")$pys)),
+                    "births_obs_mics" = list(filter(mf$observations$full_obs, survtype == "MICS")$births),
+                    
+                    "X_spike_2000_mics" = list(model.matrix(~0 + spike_2000, mf$observations$full_obs %>% filter(survtype == "MICS"))),
+                    "X_spike_1999_mics" = list(model.matrix(~0 + spike_1999, mf$observations$full_obs %>% filter(survtype == "MICS"))),
+                    "X_spike_2001_mics" = list(model.matrix(~0 + spike_2001, mf$observations$full_obs %>% filter(survtype == "MICS")))
+  )
   tmb_int$par <- c(tmb_int$par,
                    "u_tips_mics" = list(rep(0, ncol(mf$Z$Z_tips_mics)))
   )
+  tmb_int$random <- c(tmb_int$random, "u_tips_mics")
 }
 
 
@@ -147,7 +162,7 @@ if(mf$mics_toggle) {
 
 obj <-  TMB::MakeADFun(data = tmb_int$data,
                   parameters = tmb_int$par,
-                  DLL = "tmb_nb_spike_eth_b",
+                  DLL = "tmb_all_level_eth",
                   random = tmb_int$random,
                   hessian = FALSE)
 
