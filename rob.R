606d2617a274d1df5986bd61465fd68f790131c0
osymandius <bolistevens@gmail.com>
2021-07-29 15:02:53 +0100

    Update rob.R

--- rob.R
+++ rob.R
@@ -1,12 +1,16 @@
 install.packages(c("purrr","tidyverse"))
 
 devtools::install_github("vimc/orderly")
+devtools::install_github("vimc/orderly.sharepoint")
+devtools::install_github("reside-ic/spud")
 devtools::install_github("osymandius/dfertility")
 devtools::install_github("mrc-ide/naomi")
 
 library(tidyverse)
 library(purrr)
 library(orderly)
+library(orderly.sharepoint)
+library(spud)
 
 iso <- c("BDI", "BEN", "BFA", "CIV", "CMR", "COD", "COG", "GMB", "KEN", "LSO", "MLI", "MOZ", "MWI", "NGA", "SLE", "SWZ", "TCD", "TGO", "ZWE", "AGO", "ETH", "GAB", "GHA", "GIN", "LBR", "NAM", "NER", "RWA", "SEN", "TZA", "UGA", "ZMB")
 
