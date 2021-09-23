d9188b93d819b168f0aa37443f05bba2cb02c2e9
osymandius <bolistevens@gmail.com>
2021-09-23 10:56:18 +0100

    Update aaa_fit.R

--- src/aaa_fit/aaa_fit.R
+++ src/aaa_fit/aaa_fit.R
@@ -235,7 +235,7 @@ fit <- naomi::sample_tmb(fit, random_only=TRUE)
 tmb_results <- dfertility::tmb_outputs(fit, mf, areas)
 write_csv(tmb_results, "fr.csv")
 
-fit <- naomi::sample_tmb(fit, random_only=FALSE)
+# fit <- naomi::sample_tmb(fit, random_only=FALSE)
 hyper <- fit$sample %>%
   list_modify("lambda_out" = zap(), "tfr_out" = zap())
 saveRDS(hyper, "hyper.rds")
