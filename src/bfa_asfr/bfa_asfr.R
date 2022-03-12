079f06a37e7464236873f39008735da22fed78b6
osymandius <bolistevens@gmail.com>
2022-03-12 15:46:21 +0100

    fix case

---
+++ src/bfa_asfr/bfa_asfr.R
@@ -0,0 +1,60 @@
+iso3 <- "BFA"
+
+areas <- read_sf(paste0("depends/", tolower(iso3), "_areas.geojson"))
+clusters <- read.csv(paste0("depends/", tolower(iso3), "_dhs_clusters.csv"))
+source("resources/utility_funs.R")
+
+areas_wide <- spread_areas(areas)
+areas_long <- areas %>% st_drop_geometry
+
+clusters <- clusters %>%
+  mutate(DHS_survey_id = str_replace(survey_id, iso3, dhs_countries()$DHS_CountryCode[dhs_countries()$ISO3_CountryCode == iso3]))
+
+surveys <- dhs_surveys(surveyIds = unique(clusters$DHS_survey_id)) %>%
+  left_join(clusters %>% select(survey_id, DHS_survey_id) %>% distinct, by=c("SurveyId" = "DHS_survey_id"))
+
+dhs_fr <- calculate_dhs_fertility(iso3, surveys, clusters, areas_wide)
+
+### MICS DATA
+
+mics_births_to_women <- read.csv(paste0("depends/", tolower(iso3), "_mics_births_to_women.csv"))
+mics_wm <- read.csv(paste0("depends/", tolower(iso3), "_mics_women.csv"))
+
+lvl_map <- read.csv("resources/iso_mapping_fit.csv")
+lvl <- lvl_map$fertility_fit_level[lvl_map$iso3 == iso3]
+admin1_lvl <- lvl_map$admin1_level[lvl_map$iso3 == iso3]
+
+mics_fr <- calculate_mics_fertility(iso3, mics_wm, mics_births_to_women)
+
+if(exists("mics_fr")){
+  asfr <- dhs_fr$asfr %>%
+    bind_rows(mics_fr$mics_asfr)
+  
+  plot_dat <- dhs_fr$plot %>%
+    bind_rows(mics_fr$mics_plot)
+  
+} else {
+  asfr <- dhs_fr$asfr
+  plot_dat <- dhs_fr$plot
+}
+
+write_csv(asfr, paste0(tolower(iso3), "_asfr.csv"))
+
+plot <- plot_dat %>%
+  filter(variable == "tfr", value<15) %>%
+  ggplot(aes(x=period, y=value, color=survey_id)) +
+  geom_point() +
+  facet_wrap(~area_id, ncol=5) +
+  labs(y="TFR", x=element_blank(), color="Survey ID", title=paste(iso3, "| Provincial TFR")) +
+  theme_minimal() +
+  theme(
+    legend.position = "bottom",
+    text = element_text(size=14)
+  )
+
+dir.create("check")
+pdf("check/tfr_admin1.pdf", h = 12, w = 20)
+plot
+dev.off()
+
+write_csv(plot_dat, paste0(tolower(iso3), "_fr_plot.csv"))
---
+++ src/eth_asfr/eth_asfr.R
@@ -0,0 +1,53 @@
+iso3 <- "ETH"
+
+areas <- read_sf(paste0("depends/", tolower(iso3), "_areas.geojson"))
+clusters <- read.csv(paste0("depends/", tolower(iso3), "_dhs_clusters.csv"))
+source("resources/utility_funs.R")
+
+areas_wide <- spread_areas(areas)
+areas_long <- areas %>% st_drop_geometry
+
+clusters <- clusters %>%
+  filter(survey_id != "ETH2000DHS") %>%
+  mutate(DHS_survey_id = str_replace(survey_id, iso3, dhs_countries()$DHS_CountryCode[dhs_countries()$ISO3_CountryCode == iso3]))
+
+surveys <- dhs_surveys(surveyIds = unique(clusters$DHS_survey_id)) %>%
+  left_join(clusters %>% select(survey_id, DHS_survey_id) %>% distinct, by=c("SurveyId" = "DHS_survey_id")) %>%
+  filter(survey_id != "ETH2000DHS")
+
+dhs_fr <- calculate_dhs_fertility(iso3, surveys, clusters, areas_wide)
+
+if(exists("mics_fr")) {
+  
+  asfr <- dhs_fr$asfr %>%
+    bind_rows(mics_fr$mics_asfr)
+  
+  plot_dat <- dhs_fr$plot %>%
+    bind_rows(mics_fr$mics_plot)
+  
+} else {
+  asfr <- dhs_fr$asfr
+  plot_dat <- dhs_fr$plot
+}
+
+write_csv(asfr, paste0(tolower(iso3), "_asfr.csv"))
+
+plot <- plot_dat %>%
+  filter(variable == "tfr", value<15) %>%
+  ggplot(aes(x=period, y=value, color=survey_id)) +
+  geom_point() +
+  facet_wrap(~area_id, ncol=5) +
+  labs(y="TFR", x=element_blank(), color="Survey ID", title=paste(iso3, "| Provincial TFR")) +
+  theme_minimal() +
+  theme(
+    legend.position = "bottom",
+    text = element_text(size=14)
+  )
+
+dir.create("check")
+pdf("check/tfr_admin1.pdf", h = 12, w = 20)
+plot
+dev.off()
+
+write_csv(plot_dat, paste0(tolower(iso3), "_fr_plot.csv"))
+write_csv(asfr, paste0(tolower(iso3), "_asfr.csv"))
