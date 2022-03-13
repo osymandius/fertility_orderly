a1c7ed51d78bcebbfa41f84f41d21deebc732e32
osymandius <bolistevens@gmail.com>
2022-03-13 16:13:57 +0100

    Add ZAF survey

---
+++ src/zaf_survey/zaf_survey.R
@@ -0,0 +1,51 @@
+#' ISO3 country code
+iso3 <- "ZAF"
+
+areas <- read_sf("depends/zaf_areas.geojson") %>%
+  mutate(area_name = str_trim(area_name)) %>%
+  st_make_valid()
+
+areas_wide <- spread_areas(areas)
+
+surveys <- create_surveys_dhs(iso3, survey_characteristics = NULL) %>%
+  filter(as.numeric(SurveyYear) > 1994)
+
+survey_meta <- create_survey_meta_dhs(surveys)
+
+survey_region_boundaries <- create_survey_boundaries_dhs(surveys)
+
+surveys <- surveys_add_dhs_regvar(surveys, survey_region_boundaries)
+
+#' Allocate each area to survey region
+
+survey_region_areas <- allocate_areas_survey_regions(areas_wide, survey_region_boundaries)
+
+validate_survey_region_areas(survey_region_areas, survey_region_boundaries)
+
+survey_regions <- create_survey_regions_dhs(survey_region_areas)
+
+#' # Survey clusters dataset
+
+survey_clusters <- create_survey_clusters_dhs(surveys, clear_rdhs_cache = TRUE)
+
+
+#' Snap survey clusters to areas
+
+survey_clusters <- assign_dhs_cluster_areas(survey_clusters, survey_region_areas)
+
+#' 1998 DHS has no GPS coordinates. Assign clusters to areas using survey region id
+survey_clusters <- survey_clusters %>%
+  left_join(survey_regions %>% 
+              filter(survey_id == "ZAF1998DHS")) %>%
+  mutate(geoloc_area_id = ifelse(is.na(geoloc_area_id) & !is.na(survey_region_area_id), survey_region_area_id, geoloc_area_id)) %>%
+  select(-c(survey_region_name, survey_region_area_id)) 
+
+
+p_coord_check <- plot_survey_coordinate_check(survey_clusters,
+                                              survey_region_boundaries,
+                                              survey_region_areas)
+
+dir.create("check")
+pdf(paste0("check/", tolower(iso3), "_dhs-cluster-check.pdf"), h = 5, w = 7)
+p_coord_check
+dev.off()
