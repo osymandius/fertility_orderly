7f9c5db60f6c9633367180e8432de9a760489de7
osymandius <bolistevens@gmail.com>
2022-03-13 21:15:21 +0100

    Add SSD

--- src/ssd_data_areas/script.R
+++ src/ssd_data_areas/script.R
@@ -1,12 +1,62 @@
-areas <- read_sf("ssd_areas_in.geojson")
+## sf v 1.0.0 update changes to use s2 spherical geometry as default
+## This creates issues for DHS coordinate data extraction scripts
+## Revert back to planar geometry
+sf::sf_use_s2(FALSE)
 
-sf::st_write(areas, "ssd_areas.geojson", delete_dsn = TRUE)
+humdata_path <- "~/Imperial College London/HIV Inference Group - WP - Documents/Data/naomi-raw/SSD/2022-03-13 humdata-shapefile/ssd_adm_imwg_nbs_20220121.zip"
 
-# int <- sf::read_sf("src/ssd_data_areas/ssd_areas_in.geojson")[1:11,] %>%
-#   mutate(area_name = ifelse(area_name == "Central African Republic", "South Sudan", area_name)) %>%
-#   select(-X1)
-# #   mutate(center_x = 1,
-# #          center_y = 1,
-# #          naomi_level = ifelse(area_level, TRUE, FALSE))
-# # 
-# st_write(int, "src/ssd_data_areas/ssd_areas_in.geojson", delete_dsn = TRUE)
+tempdir <- tempdir()
+unzip(humdata_path, exdir = tempdir)
+admin0 <- read_sf(file.path(tempdir, "ssd_admbnda_adm0_imwg_nbs_20210924.shp")) %>%
+  rename(area_name = ADM0_EN) %>%
+  mutate(area_id = "SSD",
+         area_level = 0,
+         area_level_label = "Country",
+         spectrum_level = TRUE,
+         epp_level = TRUE,
+         naomi_level = FALSE,
+         pepfar_psnu_level = FALSE,
+         display = TRUE)
+
+admin1 <- read_sf(file.path(tempdir, "ssd_admbnda_adm1_imwg_nbs_20210924.shp")) %>%
+  rename(area_name = ADM1_EN) %>%
+  mutate(area_id = paste0("SSD_1_", row_number()))
+
+admin1 <- admin1 %>%
+  select(area_id, area_name, geometry) %>%
+  mutate(parent_area_id = "SSD",
+         area_level = 1,
+         area_level_label = "State",
+         spectrum_level = FALSE,
+         epp_level = FALSE,
+         naomi_level = FALSE,
+         pepfar_psnu_level = FALSE,
+         display = TRUE)
+
+admin2 <- read_sf(file.path(tempdir, "ssd_admbnda_adm2_imwg_nbs_20180817.shp")) %>%
+  rename(area_name = ADM2_EN) %>%
+  mutate(area_id = paste0("SSD_2_", row_number()))
+
+admin2 <- admin2 %>%
+  select(area_id, area_name, geometry, parent_area_name = ADM1_EN) %>%
+  left_join(admin1 %>% select(parent_area_name = area_name, parent_area_id = area_id) %>% st_drop_geometry()) %>%
+  select(area_id, area_name, parent_area_id) %>%
+  mutate(area_level = 2,
+         area_level_label = "County",
+         spectrum_level = FALSE,
+         epp_level = FALSE,
+         naomi_level = TRUE,
+         pepfar_psnu_level = TRUE,
+         display = TRUE)
+
+areas <- admin0 %>%
+  bind_rows(admin1, admin2) %>%
+  mutate(area_sort_order = row_number(),
+         spectrum_region_code = NA,
+         center = st_centroid(geometry),
+         center_x = do.call(rbind, center)[,1],
+         center_y = do.call(rbind, center)[,2]) %>%
+  select(area_id, area_name, area_level, parent_area_id, area_sort_order, center_x, center_y, spectrum_region_code, area_level_label, display, spectrum_level, epp_level, naomi_level, pepfar_psnu_level) %>%
+  st_make_valid()
+
+st_write(areas,"ssd_areas.geojson", delete_dsn = TRUE)
---
+++ src/ssd_survey/ssd_survey.R
@@ -0,0 +1,32 @@
+#' ISO3 country code
+iso3 <- "SSD"
+
+areas <- read_sf("depends/ssd_areas.geojson")
+# areas <- read_sf("archive/caf_data_areas/20210224-072623-9420edea/caf_areas.geojson")
+
+
+####### MICS
+
+mics_indicators <- read_csv("resources/MICS_indicators.csv") %>%
+  pivot_longer(-c(label, id, filetype), names_to = "survey_id")
+
+mics_survey_data <- create_surveys_mics(iso3, mics_indicators)
+
+fertility_mics_data <- transform_mics(mics_survey_data, mics_indicators)
+
+fertility_mics_data$hh <- fertility_mics_data$hh %>%
+  mutate(
+    mics_area_name_label = case_when(
+        mics_area_name_label == "Warap" ~ "Warrap",
+        mics_area_name_label == "Northern Bahr El Ghazal" ~ "Northern Bahr el Ghazal",
+        mics_area_name_label == "Western Bahr El Ghazal" ~ "Western Bahr el Ghazal",
+      TRUE ~ mics_area_name_label
+    )
+  ) 
+
+mics_survey_areas <- join_survey_areas(fertility_mics_data, areas)
+
+asfr_input_data <- make_asfr_inputs(mics_survey_areas, mics_survey_data)
+
+write_csv(asfr_input_data$wm, paste0(tolower(iso3), "_mics_women.csv"))
+write_csv(asfr_input_data$births_to_women, paste0(tolower(iso3), "_mics_births_to_women.csv"))
