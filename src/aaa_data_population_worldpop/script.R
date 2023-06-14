b02e5168c7c64df87316218c68fdd437d29d9cb1
osymandius <bolistevens@gmail.com>
2023-06-14 16:18:41 +0100

    Remove inference-data tasks from repo

--- src/aaa_data_population_worldpop/script.R
+++
@@ -1,5 +0,0 @@
-areas <- read_area_merged("depends/naomi_areas.geojson") %>%
-  st_make_valid()
-
-pop <- naomi_extract_worldpop(areas, iso3, years = c(2000, 2005, 2010, 2015, 2020))
-write_csv(pop, "population_worldpop_naomi.csv", na = "")
